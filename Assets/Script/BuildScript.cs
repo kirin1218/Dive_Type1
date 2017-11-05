c28fb51b3c9fd51bc41270c8c75a37bc001d1a56
kirin1218 <yukirin.my.angel@gmail.com>
2017-11-05 20:56:22 +0900

    読み込んだjsonの描画、ボールの移動など基本的な動作を再現

--- Assets/Script/BuildScript.cs
+++ Assets/Script/BuildScript.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public class BuildScript : MonoBehaviour {
     public Mesh _mesh;
     private int AreaSize;
@@ -16,35 +14,9 @@ public class BuildScript : MonoBehaviour {
         posOnStage = _posOnStage;
         posOnWorld = _posOnWorld;
     }
-    //public void MakeObject( List<Vector3> vertices, List<int> triangles, float sizeX, float sizeY, float sizeZ )
+
     public void MakeObject( List<Vector3> vertices, List<int> triangles )
     {
-
-#if false
-        this.transform.localPosition = new Vector3(10, 10, 10);
-        this.transform.localScale = new Vector3(10, 10, 10);
-        Mesh _mesh = new Mesh();
-
-        List<Vector3> listOuterFrame = new List<Vector3>();
-        List<int> listOutertriangle = new List<int>();
-
-        listOuterFrame.Add(new Vector3(5, 0, 5));
-        listOuterFrame.Add(new Vector3(5, 15, 5));
-        listOuterFrame.Add(new Vector3(5, 0, -5));
-
-        listOutertriangle.Add(0);
-        listOutertriangle.Add(1);
-        listOutertriangle.Add(2);
-        _mesh.vertices = listOuterFrame.ToArray();
-        _mesh.triangles = listOutertriangle.ToArray();
-        _mesh.RecalculateBounds();
-
-        var filter = GetComponent<MeshFilter>();
-        filter.sharedMesh = _mesh;
-
-        return;
-#endif
-#if true
         _mesh = new Mesh();
 
         List<Vector3> vertices_local = new List<Vector3>() ;
@@ -60,43 +32,6 @@ public class BuildScript : MonoBehaviour {
 
         _mesh.RecalculateBounds();
 
-        //var collider = GetComponent<MeshCollider>();
-        //collider.sharedMesh = _mesh;
-
-//this.transform.localScale = new Vector3( sizeX, sizeY, sizeZ);
-//this.transform.position = new Vector3( sizeX/2, sizeY/2, sizeZ/2 );
-
-//var collider = GetComponent<BoxCollider>();
-//collider.transform. 
-
-
-// 変更箇所 : MeshRendererからMaterialにアクセスし、Materialをセットするようにする
-//var renderer = buildObj.GetComponent<MeshRenderer>();
-//renderer.material = mateBuild[UnityEngine.Random.Range(0, 3)];
-#endif
-#if false
-        Vector3 pos = this.transform.localPosition;
-        Vector3 scale = this.transform.localScale;
-
-        _mesh = new Mesh();
-
-        List<Vector3> listOuterFrame = new List<Vector3>();
-        List<int> listOutertriangle = new List<int>();
-
-        listOuterFrame.Add(new Vector3(5, 0, 5));
-        listOuterFrame.Add(new Vector3(5, 15, 5));
-        listOuterFrame.Add(new Vector3(5, 0, -5));
-
-        listOutertriangle.Add(0);
-        listOutertriangle.Add(1);
-        listOutertriangle.Add(2);
-        _mesh.vertices = listOuterFrame.ToArray();
-        _mesh.triangles = listOutertriangle.ToArray();
-        _mesh.RecalculateBounds();
-
-        //var filter = GetComponent<MeshFilter>();
-        //filter.sharedMesh = _mesh;
-#endif
     }
 
     // Use this for initialization
@@ -112,16 +47,7 @@ public class BuildScript : MonoBehaviour {
 
         if (_mesh != null)
         {
-            if( filter.sharedMesh != null)
-            {
-                //filter.sharedMesh = _mesh;
-            }
             Graphics.DrawMesh(_mesh, this.transform.position, Quaternion.identity, renderer.material, 0);
-        }
-        else
-        {
-            int a;
-            a = 3;
         }
     }
 
--- Assets/Script/StageScript.cs
+++ Assets/Script/StageScript.cs
@@ -16,7 +16,6 @@ public class StageScript : MonoBehaviour {
 
     public void AddBuildings( int x, int y )
     {
-#if true
         string filePath;
         float centerX = AreaSize * x;
         float centerY = AreaSize * y;
@@ -33,58 +32,6 @@ public class StageScript : MonoBehaviour {
 
         for (int i = 0; i < mapinfo.Count(); i++)
         {
-#if false
-            GameObject buildObj = (GameObject)Instantiate(
-                                                buildprefab2,
-                                                new Vector3(0, 0, 0),
-                                                Quaternion.identity
-                                                );
-            GameObject emptyObject = new GameObject();
-            emptyObject.transform.rotation = transform.rotation;
-            emptyObject.transform.parent = transform;
-            buildObj.transform.parent = emptyObject.transform;
-
-            BuildInfo build = mapinfo.List[i];
-
-            float minX = 10000, maxX = 0;
-            float minY = 10000, maxY = 0;
-            float minZ = 10000, maxZ = 0;
-
-
-            for (int j = 0; j < build.CountVertex3(); j++)
-            {
-                if (minX > build.getVertex3[j].x)
-                {
-                    minX = build.getVertex3[j].x;
-                }
-                if (maxX < build.getVertex3[j].x)
-                {
-                    maxX = build.getVertex3[j].x;
-                }
-                if (minY > build.getVertex3[j].y)
-                {
-                    minY = build.getVertex3[j].y;
-                }
-                if (maxY < build.getVertex3[j].y)
-                {
-                    maxY = build.getVertex3[j].y;
-                }
-                if (minZ > build.getVertex3[j].z)
-                {
-                    minZ = build.getVertex3[j].z;
-                }
-                if (maxZ < build.getVertex3[j].z)
-                {
-                    maxZ = build.getVertex3[j].z;
-                }
-            }
-
-            float sizeX = maxX - minX;
-            float sizeY = maxY - minY;
-            float sizeZ = maxZ - minZ;
-            buildObj.transform.localScale = new Vector3(sizeX, sizeY, sizeZ);
-            buildObj.transform.localPosition = new Vector3(minX - centerX + (sizeX / 2), (maxY + minY) / 2, minZ - centerY + (sizeZ / 2));
-#else
             GameObject buildObj = (GameObject)Instantiate(
                                                  buildprefab,
                                                  new Vector3(0, 0, 0),
@@ -135,37 +82,8 @@ public class StageScript : MonoBehaviour {
             float sizeY = maxY - minY;
             float sizeZ = maxZ - minZ;
             buildObj.transform.localScale = new Vector3(sizeX, sizeY, sizeZ);
-#if false
-            var vertices = new List<Vector3>();
-            var triangles = new List<int>();
-
-            for (int j = 0; j < build.CountVertex3(); j++)
-            {
-                vertices.Add(new Vector3((build.getVertex3[j].x - centerX)/1000, (build.getVertex3[j].y)/1000, (build.getVertex3[j].z - centerY)/1000));
-            }
-
-            for (int k = 0; k < build.CountPolygon3(); k++)
-            {
-                Polygon3 poly = build.getPolygon3[k];
-                triangles.Add(poly.i1);
-                triangles.Add(poly.i2);
-                triangles.Add(poly.i3);
-            }
-
-            Mesh _mesh = new Mesh();
-
-            // (4) Meshに頂点情報を代入
-            _mesh.vertices = vertices.ToArray();
-            _mesh.triangles = triangles.ToArray();
-            //        _mesh.normals = normals.ToArray();
-
-            _mesh.RecalculateBounds();
-
-            var filter = buildObj.GetComponent<MeshFilter>();
-            filter.sharedMesh = _mesh;
-#endif
             buildObj.transform.localPosition = new Vector3(minX - centerX + (sizeX / 2), (maxY + minY) / 2, minZ - centerY + (sizeZ / 2));
-#endif
+
             var vertices = new List<Vector3>();
             var triangles = new List<int>();
 
@@ -189,125 +107,12 @@ public class StageScript : MonoBehaviour {
         }
 
         transform.position = new Vector3(x * AreaSize, 0, y * AreaSize);
-#endif
-
-#if false
-        string filePath;
-        float centerX = AreaSize * x;
-        float centerY = AreaSize * y;
-
-        filePath = Application.dataPath + "/json/mapinfo_" + x.ToString() + "x" + y.ToString() + ".json";
-
-        if (!File.Exists(filePath))
-        {
-            return;
-        }
-
-        JsonController mapinfoJson = GetComponent<JsonController>();
-        mapinfo = mapinfoJson.LoadFromJson(filePath);
-
-        for (int i = 0; i < mapinfo.Count(); i++)
-        {
-            GameObject buildObj = (GameObject)Instantiate(
-                                                buildprefab,
-                                                new Vector3(0, 0, 0),
-                                                Quaternion.identity
-                                                );
-
-            GameObject emptyObject = new GameObject();
-            emptyObject.transform.rotation = transform.rotation;
-            emptyObject.transform.parent = transform;
-            buildObj.transform.parent = emptyObject.transform;
-
-
-            BuildInfo build = mapinfo.List[i];
-
-            float minX = 10000, maxX = 0;
-            float minY = 10000, maxY = 0;
-            float minZ = 10000, maxZ = 0;
-            //float x;
-            //float y;
-            //float z;
-            //float centerX;
-            //float centerY;
-            //float centerZ;
-
-            for (int j = 0; j < build.CountVertex3(); j++)
-            {
-                if (minX > build.getVertex3[j].x)
-                {
-                    minX = build.getVertex3[j].x;
-                }
-                if (maxX < build.getVertex3[j].x)
-                {
-                    maxX = build.getVertex3[j].x;
-                }
-                if (minY > build.getVertex3[j].y)
-                {
-                    minY = build.getVertex3[j].y;
-                }
-                if (maxY < build.getVertex3[j].y)
-                {
-                    maxY = build.getVertex3[j].y;
-                }
-                if (minZ > build.getVertex3[j].z)
-                {
-                    minZ = build.getVertex3[j].z;
-                }
-                if (maxZ < build.getVertex3[j].z)
-                {
-                    maxZ = build.getVertex3[j].z;
-                }
-            }
-
-            buildObj.transform.position = new Vector3((minX + maxX) / 2, (minY + maxY) / 2, (minZ + maxZ) / 2);
-
-            continue;
-
-           // break;
-
-            var vertices = new List<Vector3>();
-            var triangles = new List<int>();
-            //var normals = new List<Vector3>();
-
-
-            for (int j = 0; j < build.CountVertex3(); j++)
-            {
-                vertices.Add(new Vector3(build.getVertex3[j].x - centerX, build.getVertex3[j].y, build.getVertex3[j].z - centerY ));
-            }
-
-            for (int k = 0; k < build.CountPolygon3(); k++)
-            {
-                Polygon3 poly = build.getPolygon3[k];
-                triangles.Add(poly.i1);
-                triangles.Add(poly.i2);
-                triangles.Add(poly.i3);
-            }
-
-
-            buildObj.transform.localScale = new Vector3( ( maxX - minX )/ AreaSize, (maxY - minY)/ AreaSize, (maxZ - minZ)/ AreaSize);
-            buildObj.transform.position = new Vector3( (minX + ( maxX - minX ) /2), (minY + (maxY - minY)/2), (minZ + (maxZ - minZ)/2));
-
-            BuildScript bs = buildObj.GetComponent<BuildScript2>();
-            bs.MakeObject(vertices, triangles, maxX - minX, maxY - minY, maxZ - minZ );
-
-
-
-
-            //break;
-        }
-
-        transform.position = new Vector3( x * AreaSize, 0, y * AreaSize );
-#endif
-        }
+    }
 
     // Use this for initialization
     void Start () {
         //  大きさを100x100にする
         this.transform.localScale = new Vector3( AreaSize, 0.1f, AreaSize );
-
-        //  建物を建てていくよ
-        //AddBuildings(0, 0);
     }
 	
 	// Update is called once per frame
